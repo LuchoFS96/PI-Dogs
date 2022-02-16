5cf577c6debfbd1f8515a8c674adbb7b282dd229
Silvia Alvarez <silvia@MacBook-Air-de-Silvia.local>
2022-02-16 15:26:45 -0300

    Controlled Forms

--- client/src/components/createDog/index.jsx
+++ client/src/components/createDog/index.jsx
@@ -7,6 +7,7 @@ import "./index.css";
 export default function CreateDog(props) {
   let dispatch = useDispatch();
   let i = 0;
+
   const [state, setState] = useState({
     name: "",
     min_height: "",
@@ -31,65 +32,74 @@ export default function CreateDog(props) {
       ...state,
       [e.target.name]: e.target.value,
     });
-    // setErrors(
-    //   validate({
-    //     ...state,
-    //     [e.target.name]: e.target.value,
-    //   })
-    // );
+    setErrors(
+      validate({
+        ...state,
+        [e.target.name]: e.target.value,
+      })
+    );
   }
 
   function handleSubmit(e) {
-    // if (
-    //   errors.name !== undefined ||
-    //   errors.height !== undefined ||
-    //   errors.weight !== undefined ||
-    //   errors.life_span !== undefined
-    // ) {
-    //   e.preventDefault();
-    //   return alert("Sorry, all fields are required except image");
-    // } else if (
-    //   state.name === "" ||
-    //   state.height === "" ||
-    //   state.weight === "" ||
-    //   state.life_span === ""
-    // ) {
-    //   e.preventDefault();
-    //   return alert("Sorry, all fields are required except image");
-    // } else if (
-    //   isNaN(parseInt(state.height)) ||
-    //   isNaN(parseInt(state.weight)) ||
-    //   isNaN(parseInt(state.life_span))
-    // ) {
-    //   e.preventDefault();
+    if (
+      errors.name !== undefined ||
+      errors.min_height !== undefined ||
+      errors.max_height !== undefined ||
+      errors.min_weight !== undefined ||
+      errors.max_weight !== undefined ||
+      errors.min_life_span !== undefined ||
+      errors.max_life_span !== undefined
+    ) {
+      e.preventDefault();
+      return alert("Sorry, all fields are required except image");
+    } else if (
+      state.name === "" ||
+      state.min_height === "" ||
+      state.max_height === "" ||
+      state.min_weight === "" ||
+      state.max_weight === "" ||
+      state.min_life_span === "" ||
+      state.max_life_span === ""
+    ) {
+      e.preventDefault();
+      return alert("Sorry, all fields are required except image");
+    } else if (
+      isNaN(parseInt(state.min_height)) ||
+      isNaN(parseInt(state.max_height)) ||
+      isNaN(parseInt(state.min_weight)) ||
+      isNaN(parseInt(state.max_weight)) ||
+      isNaN(parseInt(state.min_life_span)) ||
+      isNaN(parseInt(state.max_life_span))
+    ) {
+      e.preventDefault();
 
-    //   return alert("Sorry, please fill out the required fields correctly");
-    // } else {
-    const dog = {
-      name: state.name,
-      height: `${state.min_height} - ${state.max_height}`,
-      weight: `${state.min_weight} - ${state.max_weight}`,
-      life_span: `${state.min_life_span} - ${state.max_life_span}`,
-      image:
-        state.image !== ""
-          ? state.image
-          : "https://previews.123rf.com/images/red33/red331112/red33111200014/11546849-skizzieren-sie-doodle-crazy-verr%C3%BCckt-puppy-dog-vektor-illustration.jpg",
-      temperament: [...state.temperaments],
-    };
-    console.log(dog);
-    setState({
-      name: "",
-      min_height: "",
-      max_height: "",
-      min_weight: "",
-      max_weight: "",
-      min_life_span: "",
-      max_life_span: "",
-      image: "",
-      temperaments: [],
-    });
-    dispatch(createDog(dog));
-    // }
+      return alert("Sorry, please fill out the required fields correctly");
+    } else {
+      const dog = {
+        name: state.name,
+        height: `${state.min_height} - ${state.max_height}`,
+        weight: `${state.min_weight} - ${state.max_weight}`,
+        life_span: `${state.min_life_span} - ${state.max_life_span}`,
+        image:
+          state.image !== ""
+            ? state.image
+            : "https://previews.123rf.com/images/red33/red331112/red33111200014/11546849-skizzieren-sie-doodle-crazy-verr%C3%BCckt-puppy-dog-vektor-illustration.jpg",
+        temperament: [...state.temperaments],
+      };
+      console.log(dog);
+      setState({
+        name: "",
+        min_height: "",
+        max_height: "",
+        min_weight: "",
+        max_weight: "",
+        min_life_span: "",
+        max_life_span: "",
+        image: "",
+        temperaments: [],
+      });
+      dispatch(createDog(dog));
+    }
   }
 
   function addTemp(e) {
@@ -107,18 +117,36 @@ export default function CreateDog(props) {
       errors.name = "Name is missing";
     } else if (expresion.test(input.name) === false) {
       errors.name = "Name invalid";
-    } else if (!input.height) {
-      errors.height = "Min height is missing";
-    } else if (input.height <= 0) {
-      errors.height = "height cannot be negative or zero";
-    } else if (!input.weight) {
-      errors.weight = "Min weight is missing";
-    } else if (input.weight <= 0) {
-      errors.weight = "Weight can not be less";
-    } else if (!input.life_span) {
-      errors.life_span = "Min life is missing";
-    } else if (input.life_span <= 0) {
-      errors.life_span = "life years cannot be negative or zero";
+    } else if (!input.min_height) {
+      errors.min_height = "Min height is missing";
+    } else if (input.min_height <= 0) {
+      errors.min_height = "height cannot be negative or zero";
+    } else if (!input.max_height) {
+      errors.max_height = "Max height is missing";
+    } else if (input.max_height <= 0) {
+      errors.max_height = "height cannot be negative or zero";
+    } else if (parseInt(input.min_height) >= parseInt(input.max_height)) {
+      errors.max_height = "Max height can not be less";
+    } else if (!input.min_weight) {
+      errors.min_weight = "Min weight is missing";
+    } else if (input.min_weight <= 0) {
+      errors.min_weight = "Weight can not be less";
+    } else if (!input.max_weight) {
+      errors.max_weight = "Max weight is missing";
+    } else if (input.max_weight <= 0) {
+      errors.max_weight = "Weight cannot be negative or zero";
+    } else if (parseInt(input.min_weight) >= parseInt(input.max_weight)) {
+      errors.max_weight = "Max weight can not be less";
+    } else if (!input.min_life_span) {
+      errors.min_life_span = "Min life is missing";
+    } else if (input.min_life_span <= 0) {
+      errors.min_life_span = "life years cannot be negative or zero";
+    } else if (!input.max_life_span) {
+      errors.max_life_span = "Max life is missing";
+    } else if (input.max_life_span <= 0) {
+      errors.max_life_span = "life years cannot be negative or zero";
+    } else if (parseInt(input.min_life_span) >= parseInt(input.max_life_span)) {
+      errors.max_life_span = "Max life can not be less";
     }
     return errors;
   }
@@ -136,6 +164,7 @@ export default function CreateDog(props) {
             value={state.name}
             onChange={(e) => handleChange(e)}
           ></input>
+          {errors.name && <p className="pop-up">{errors.name}</p>}
 
           <br />
 
@@ -147,6 +176,7 @@ export default function CreateDog(props) {
             value={state.min_height}
             onChange={(e) => handleChange(e)}
           ></input>
+          {errors.min_height && <p className="pop-up">{errors.min_height}</p>}
 
           <br />
 
@@ -158,6 +188,7 @@ export default function CreateDog(props) {
             value={state.max_height}
             onChange={(e) => handleChange(e)}
           ></input>
+          {errors.max_height && <p className="pop-up">{errors.max_height}</p>}
 
           <br />
 
@@ -169,6 +200,7 @@ export default function CreateDog(props) {
             value={state.min_weight}
             onChange={(e) => handleChange(e)}
           ></input>
+          {errors.min_weight && <p className="pop-up">{errors.min_weight}</p>}
 
           <br />
 
@@ -180,6 +212,7 @@ export default function CreateDog(props) {
             value={state.max_weight}
             onChange={(e) => handleChange(e)}
           ></input>
+          {errors.max_weight && <p className="pop-up">{errors.max_weight}</p>}
 
           <br />
 
@@ -191,6 +224,9 @@ export default function CreateDog(props) {
             value={state.min_life_span}
             onChange={(e) => handleChange(e)}
           ></input>
+          {errors.min_life_span && (
+            <p className="pop-up">{errors.min_life_span}</p>
+          )}
 
           <br />
 
@@ -202,6 +238,9 @@ export default function CreateDog(props) {
             value={state.max_life_span}
             onChange={(e) => handleChange(e)}
           ></input>
+          {errors.max_life_span && (
+            <p className="pop-up">{errors.max_life_span}</p>
+          )}
 
           <br />
 
